fec120cadaae2f56da776684ae163a181552d2a8
matthernandez <matt.hernandez@idean.com>
2019-02-28 11:03:31 -0600

    Updates

---
+++ precache-manifest.db0416be5011fbb7adb10fc46ea22907.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdab64a45de3e313ef5ddc044c8fd0ed",
+    "url": "/vacation-tips/static/media/small.cdab64a4.jpg"
+  },
+  {
+    "revision": "4d43aafc73214d525698",
+    "url": "/vacation-tips/static/js/runtime~main.4d43aafc.js"
+  },
+  {
+    "revision": "afb1fdea43fc645ebaab",
+    "url": "/vacation-tips/static/js/main.afb1fdea.chunk.js"
+  },
+  {
+    "revision": "722ba0386ba0f026efb3",
+    "url": "/vacation-tips/static/js/2.722ba038.chunk.js"
+  },
+  {
+    "revision": "afb1fdea43fc645ebaab",
+    "url": "/vacation-tips/static/css/main.bd3fd81a.chunk.css"
+  },
+  {
+    "revision": "588eda3798531dad37e98cf57eb5b748",
+    "url": "/vacation-tips/index.html"
+  }
+];

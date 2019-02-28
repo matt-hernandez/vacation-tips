d65c8fcdf243d2ffcf38f041b1e37cdfef1a20bf
matthernandez <matt.hernandez@idean.com>
2019-02-28 13:05:41 -0600

    Add changing width to nav menu on smaller screen sizes

--- src/components/menu.js
+++ src/components/menu.js
@@ -19,26 +19,29 @@ function Menu({ activeIndex }) {
   };
 
   return (
-    <div className="menu">
-      <button className="menu-toggle" onClick={togglePosition}>
-        <span className="hamburger-stick"/>
-        <span className="hamburger-stick"/>
-        <span className="hamburger-stick"/>
-      </button>
-      <nav className={menuPosition}>
-        <button className="close-menu" onClick={closeMenu}>
-          &times;
+    <>
+      {menuPosition === 'in' && <div className="menu-close-overlay" onClick={closeMenu} />}
+      <div className="menu">
+        <button className="menu-toggle" onClick={togglePosition}>
+          <span className="hamburger-stick"/>
+          <span className="hamburger-stick"/>
+          <span className="hamburger-stick"/>
         </button>
-        <ul>
-          { journey.map((page, i) => (
-            <li key={page.url}>
-              {i !== activeIndex && <Link to={page.url}>{page.title}</Link>}
-              {i === activeIndex && <button className="fake-link">{page.title}</button>}
-            </li>
-          )) }
-        </ul>
-      </nav>
-    </div>
+        <nav className={menuPosition}>
+          <button className="close-menu" onClick={closeMenu}>
+            &times;
+          </button>
+          <ul>
+            { journey.map((page, i) => (
+              <li key={page.url}>
+                {i !== activeIndex && <Link to={page.url}>{page.title}</Link>}
+                {i === activeIndex && <button className="fake-link">{page.title}</button>}
+              </li>
+            )) }
+          </ul>
+        </nav>
+      </div>
+    </>
   );
 }
 
